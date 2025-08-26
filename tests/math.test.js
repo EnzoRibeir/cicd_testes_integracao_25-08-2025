3338daa5f5847c47d3dbfcd649494a444e439f82
Enzo Ribeiro <160975894+EnzoRibeir@users.noreply.github.com>
2025-08-26 00:43:57 +0000

    Add additional test cases for arithmetic functions

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,3 +2,15 @@ const  math = require('../math');
 test ('soma 2 + 3 igual a 5', () => {
     expect(math.add(2,3)).toBe(5);
 });
+
+test ('subtrai 3 - 1 igual a 2', () => {
+    expect(math.subtract(3,1)).toBe(2);
+});
+
+test ('multiplica 2 * 3 igual a 6', () => {
+    expect(math.multiply(2,3)).toBe(6);
+});
+
+test ('divide 6 / 2 igual a 3', () => {
+    expect(math.divide(6,2)).toBe(3);
+});
